2788ae531e4b9cf02c1118285127cd9b567fad1c
Abdallah <fafafavd@gmail.com>
2024-07-07 17:26:41 +0300

    server can read yaml file

--- server.py
+++ server.py
@@ -1,18 +1,12 @@
 import yaml
 import os
 
-class Server:
-    def __init__(self):
-        config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'config.yaml')
-        with open(config_path, 'r') as file:
-            self.config = yaml.safe_load(file)
+config_path = os.path.join(os.path.dirname(
+    os.path.abspath(__file__)), '..', 'config.yaml')
+with open(config_path, 'r') as file:
+    config = yaml.safe_load(file)
 
-    def run(self):
-        if self.config.get('run_localhost'):
-            print("Server running on localhost")
-        else:
-            print("Server running remotely")
-
-if __name__ == "__main__":
-    server = Server()
-    server.run()
+if config.get('run_localhost'):
+    print("Server running on localhost")
+else:
+    print("Server running remotely")
