6969ae43dfda6c4c16e03ad28f36368d18eb2ecb
Abdallah <fafafavd@gmail.com>
2024-07-07 14:13:26 +0300

    added yaml config

--- server.py
+++ server.py
@@ -1,9 +1,18 @@
+import yaml
+import os
+
 class Server:
-    def __init__(self, config):
-        self.config = config
+    def __init__(self):
+        config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'config.yaml')
+        with open(config_path, 'r') as file:
+            self.config = yaml.safe_load(file)
 
     def run(self):
         if self.config.get('run_localhost'):
             print("Server running on localhost")
         else:
             print("Server running remotely")
+
+if __name__ == "__main__":
+    server = Server()
+    server.run()
